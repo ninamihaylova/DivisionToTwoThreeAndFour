4db15eb0952183b048f400c9a447e201ca2f29b1
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-17 20:11:35 +0300

    Initial commit

---
+++ DivisionToTwoThreeAndFour/Program.cs
@@ -0,0 +1,36 @@
+﻿class DivisionToTwoThreeAndFour
+{
+    public static void Main (string[]args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int divideBy2 = 0;
+        int divideBy3 = 0;
+        int divideBy4 = 0;
+        
+        for (int i = 0; i < n; i++)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+                divideBy2++;
+
+            if (number % 3 == 0)
+                divideBy3++;
+            if (number % 4 == 0)
+                divideBy4++;
+        }
+
+        double percentBy2 = (double) divideBy2 / n * 100;
+        double percentBy3 = (double) divideBy3 / n * 100;
+        double percentBy4 = (double) divideBy4 / n * 100;
+
+        Console.WriteLine($"{percentBy2:F2}%");
+        Console.WriteLine($"{percentBy3:F2}%");
+        Console.WriteLine($"{percentBy4:F2}%");
+        
+
+
+
+    }
+    }
